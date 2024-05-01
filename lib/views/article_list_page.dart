102541c7b60a55c1cd0e1dd1c5ceafc738dbb679
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-01 10:24:07 +0700

    add http packages

--- lib/views/article_list_page.dart
+++ lib/views/article_list_page.dart
@@ -6,35 +6,34 @@ import 'package:news_app/model/article.dart';
 import 'package:news_app/views/article_detail_page.dart';
 import 'package:news_app/widgets/platform_widget.dart';
 
-
 class ArticleListPage extends StatelessWidget {
   const ArticleListPage({super.key});
 
   List<Article> parseArticles(String? json) {
-      if(json == null) {
-        return[];
-      }
+    if (json == null) {
+      return [];
+    }
 
-      final List parsed = jsonDecode(json);
-      return parsed.map((json) => Article.fromJson(json)).toList();
-     }
+    final List parsed = jsonDecode(json);
+    return parsed.map((json) => Article.fromJson(json)).toList();
+  }
 
   FutureBuilder<String> _buildList(BuildContext context) {
-    return FutureBuilder(future: 
-    DefaultAssetBundle.of(context).loadString('assets/article.json'),
-    builder: (context, snapshot) {
-      final List<Article> articles = parseArticles(snapshot.data);
-      return ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          return _buildArticleItem(context, articles[index]);
-        },
-      );
-    }, 
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString('assets/article.json'),
+      builder: (context, snapshot) {
+        final List<Article> articles = parseArticles(snapshot.data);
+        return ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            return _buildArticleItem(context, articles[index]);
+          },
+        );
+      },
     );
   }
 
-  Widget _buildArticleItem(BuildContext context, Article article){
+  Widget _buildArticleItem(BuildContext context, Article article) {
     return Material(
       child: ListTile(
         contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
@@ -43,43 +42,40 @@ class ArticleListPage extends StatelessWidget {
           child: Image.network(
             article.urlToImage,
             width: 100,
-            errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
+            errorBuilder: (ctx, error, _) =>
+                const Center(child: Icon(Icons.error)),
           ),
         ),
         title: Text(article.title),
         subtitle: Text(article.author),
         onTap: () {
           Navigator.pushNamed(context, ArticleDetailPage.routeName,
-          arguments: article);
+              arguments: article);
         },
       ),
     );
   }
 
-     Widget _buildAndroid(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('News App'),
-        ),
-        body: _buildList(context),
-      );
-     }
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: _buildList(context),
+    );
+  }
 
-     Widget _buildIos(BuildContext context){
-      return CupertinoPageScaffold(
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
           middle: Text('News App'),
           transitionBetweenRoutes: false,
-          ),
-          child: _buildList(context)
-      );
-     }
+        ),
+        child: _buildList(context));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return PlatformWidget(
-      androidBuilder: _buildAndroid, 
-      iosBuilder: _buildIos);
+    return PlatformWidget(androidBuilder: _buildAndroid, iosBuilder: _buildIos);
   }
 }
-
